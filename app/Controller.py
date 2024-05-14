1019b2eae5329edd74a2d01efbf01a5d8f43484f
jacob <jacobbennett1603@gmail.com>
2024-05-14 09:29:05 +0800

    Added different appearances for percentage bars for edge cases. The big percentage bar now has no divider when the bar is full. The small bars on the account page are now grey when no-one has voted.

--- app/Controller.py
+++ app/Controller.py
@@ -4,4 +4,8 @@ def bar_init(poll):
         bar["left"] = "width:" + str(poll["left%"]) + "%"
         bar["right"] = "width:" + str(poll["right%"]) + "%"
         bar["divider"] = "left:" + str(poll["left%"] - 0.5) + "%"
+        if poll["left%"] == 0 or poll["left%"] == 100:
+                bar["ShowDivider"] = False
+        else:
+                bar["ShowDivider"] = True
         return bar
