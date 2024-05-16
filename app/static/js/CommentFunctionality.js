076c9f92d7f9ef647923ed7aac7d478efdfcb49b
jacob <jacobbennett1603@gmail.com>
2024-05-16 19:42:34 +0800

    Got the cancel button working, now deletes contents of the comment field not yet submitted. Done through onclick event on cancel button.

--- app/static/js/CommentFunctionality.js
+++ app/static/js/CommentFunctionality.js
@@ -1,10 +1,17 @@
 $(document).ready(() =>{
+    // Checks if comment field is empty, hides the comment buttons when empty, displays them when user enters message
     $("#CommentContent").keyup(function(){
         if($(this).val().length <= 0){
             $("#commentCreationButtons").css("display", "none");
         }
-        else if($("#commentCreationButtons").css("display") == "none"){
+        else{
             $("#commentCreationButtons").css("display", "block");
         }
+    });
+    // Deletes the current contents of the comment field when the user presses the cancel button
+    $("#commentCancel").click(function(){
+        event.preventDefault();
+        $("#CommentContent").val("");
+        $("#commentCreationButtons").css("display", "none");
     })
 });
