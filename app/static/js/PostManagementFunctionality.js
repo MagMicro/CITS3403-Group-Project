f17a1fd12019ee4345f91b13b8763d185f29d1b0
jacob <jacobbennett1603@gmail.com>
2024-05-18 13:54:22 +0800

    Fixed bug where redirect id was incorrectly defined for user posts on account page.

--- app/static/js/PostManagementFunctionality.js
+++ app/static/js/PostManagementFunctionality.js
@@ -1,2 +1,2 @@
 // Opens new window with a given post when a user clicks on it
- $(".postContent").click(function(){window.open("/Poll/" + $(this).parent().attr("id"));});
+ $(".postContent").click(function(){window.open("/Poll/" + $("#item_ID").val());});
