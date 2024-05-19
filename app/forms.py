24e7bbdba4e31c2ace7be0b4830234ca2e61466a
jacob <jacobbennett1603@gmail.com>
2024-05-19 12:11:23 +0800

    Fixed graphical glitch on small poll bars where incrrect colour was shown. Emphasised last line of about page more.

--- app/forms.py
+++ app/forms.py
@@ -37,7 +37,7 @@ class AccountDeletion(FlaskForm):
 class PollSearch(FlaskForm):
     tags = ["Food", "Drink", "Sports", "Fashion", "Makeup", "Subject", "Video Games", "Anime", "Board Games" , "Card Games",
             "Animals", "Insects", "People", "Places", "City", "Country", "Film", "TV", "Novels", "Abilities", "Historical", 
-            "Superheroes", "Villans", "Fiction", "Non-Fiction", "Sci-Fi", "Crime", "Horror", "Comedy" "Fantasy", "Cars",
+            "Superheroes", "Villans", "Fiction", "Non-Fiction", "Sci-Fi", "Crime", "Horror", "Comedy", "Fantasy", "Cars",
             "Military", "Romance", "Franchise", "Corporation", "Transport", "Aviation", "Navel", "Colour", "Programming"]
     SearchBar = StringField()
     SearchPrompt = StringField("Prompt: ")
